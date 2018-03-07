44d20d76c139c61de680947a08785a767512db02
elmullin <emily.mullins@comcast.net>
2018-03-07 09:26:34 -0800

    updates

---
+++ script.js
@@ -0,0 +1,69 @@
+function displayDiv1() {
+    var first = document.getElementById("imagecontent1")
+    var second = document.getElementById("imagecontent2")
+    var third = document.getElementById("imagecontent3")
+    var fourth = document.getElementById("imagecontent4")
+    var fifth = document.getElementById("imagecontent5")
+
+    first.style.zIndex = 10;
+    second.style.zIndex = 0;
+    third.style.zIndex = 0;
+    fourth.style.zIndex = 0;
+    fifth.style.zIndex = 0;
+}
+
+function displayDiv2() {
+    var first = document.getElementById("imagecontent1")
+    var second = document.getElementById("imagecontent2")
+    var third = document.getElementById("imagecontent3")
+    var fourth = document.getElementById("imagecontent4")
+    var fifth = document.getElementById("imagecontent5")
+
+    first.style.zIndex = 0;
+    second.style.zIndex = 10;
+    third.style.zIndex = 0;
+    fourth.style.zIndex = 0;
+    fifth.style.zIndex = 0;
+}
+
+function displayDiv3() {
+    var first = document.getElementById("imagecontent1")
+    var second = document.getElementById("imagecontent2")
+    var third = document.getElementById("imagecontent3")
+    var fourth = document.getElementById("imagecontent4")
+    var fifth = document.getElementById("imagecontent5")
+
+    first.style.zIndex = 0;
+    second.style.zIndex = 0;
+    third.style.zIndex = 10;
+    fourth.style.zIndex = 0;
+    fifth.style.zIndex = 0;
+}
+
+function displayDiv4() {
+    var first = document.getElementById("imagecontent1")
+    var second = document.getElementById("imagecontent2")
+    var third = document.getElementById("imagecontent3")
+    var fourth = document.getElementById("imagecontent4")
+    var fifth = document.getElementById("imagecontent5")
+
+    first.style.zIndex = 0;
+    second.style.zIndex = 0;
+    third.style.zIndex = 0;
+    fourth.style.zIndex = 10;
+    fifth.style.zIndex = 0;
+}
+
+function displayDiv5() {
+    var first = document.getElementById("imagecontent1")
+    var second = document.getElementById("imagecontent2")
+    var third = document.getElementById("imagecontent3")
+    var fourth = document.getElementById("imagecontent4")
+    var fifth = document.getElementById("imagecontent5")
+
+    first.style.zIndex = 0;
+    second.style.zIndex = 0;
+    third.style.zIndex = 0;
+    fourth.style.zIndex = 0;
+    fifth.style.zIndex = 10;
+}
