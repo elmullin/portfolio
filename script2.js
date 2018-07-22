10ba535d86554fafe36b65768d8dd4819e5d6d8b
elmullin <emily.mullins@comcast.net>
2018-07-22 12:13:11 -0700

    portfolio

---
+++ script2.js
@@ -0,0 +1,129 @@
+function displayDiv1() {
+    var first = document.getElementById("imagecontent1")
+    var second = document.getElementById("imagecontent2")
+    var third = document.getElementById("imagecontent3")
+    var fourth = document.getElementById("imagecontent4")
+    var fifth = document.getElementById("imagecontent5")
+
+    var firsti = document.getElementById("infocontent1")
+    var secondi = document.getElementById("infocontent2")
+    var thirdi = document.getElementById("infocontent3")
+    var fourthi = document.getElementById("infocontent4")
+    var fifthi = document.getElementById("infocontent5")
+
+    first.style.display = "block";
+    second.style.display = "none";
+    third.style.display = "none";
+    fourth.style.display = "none";
+    fifth.style.display = "none";
+
+    firsti.style.display = "block";
+    secondi.style.display = "none";
+    thirdi.style.display = "none";
+    fourthi.style.display = "none";
+    fifthi.style.display = "none";
+}
+
+function displayDiv2() {
+    var first = document.getElementById("imagecontent1")
+    var second = document.getElementById("imagecontent2")
+    var third = document.getElementById("imagecontent3")
+    var fourth = document.getElementById("imagecontent4")
+    var fifth = document.getElementById("imagecontent5")
+
+    var firsti = document.getElementById("infocontent1")
+    var secondi = document.getElementById("infocontent2")
+    var thirdi = document.getElementById("infocontent3")
+    var fourthi = document.getElementById("infocontent4")
+    var fifthi = document.getElementById("infocontent5")
+
+    first.style.display = "none";
+    second.style.display = "block";
+    third.style.display = "none";
+    fourth.style.display = "none";
+    fifth.style.display = "none";
+
+    firsti.style.display = "none";
+    secondi.style.display = "block";
+    thirdi.style.display = "none";
+    fourthi.style.display = "none";
+    fifthi.style.display = "none";
+}
+
+function displayDiv3() {
+    var first = document.getElementById("imagecontent1")
+    var second = document.getElementById("imagecontent2")
+    var third = document.getElementById("imagecontent3")
+    var fourth = document.getElementById("imagecontent4")
+    var fifth = document.getElementById("imagecontent5")
+
+    first.style.display = "none";
+    second.style.display = "none";
+    third.style.display = "block";
+    fourth.style.display = "none";
+    fifth.style.display = "none";
+
+    var firsti = document.getElementById("infocontent1")
+    var secondi = document.getElementById("infocontent2")
+    var thirdi = document.getElementById("infocontent3")
+    var fourthi = document.getElementById("infocontent4")
+    var fifthi = document.getElementById("infocontent5")
+
+    firsti.style.display = "none";
+    secondi.style.display = "none";
+    thirdi.style.display = "block";
+    fourthi.style.display = "none";
+    fifthi.style.display = "none";
+}
+
+function displayDiv4() {
+    var first = document.getElementById("imagecontent1")
+    var second = document.getElementById("imagecontent2")
+    var third = document.getElementById("imagecontent3")
+    var fourth = document.getElementById("imagecontent4")
+    var fifth = document.getElementById("imagecontent5")
+
+    first.style.display = "none";
+    second.style.display = "none";
+    third.style.display = "none";
+    fourth.style.display = "block";
+    fifth.style.display = "none";
+
+    var firsti = document.getElementById("infocontent1")
+    var secondi = document.getElementById("infocontent2")
+    var thirdi = document.getElementById("infocontent3")
+    var fourthi = document.getElementById("infocontent4")
+    var fifthi = document.getElementById("infocontent5")
+
+    firsti.style.display = "none";
+    secondi.style.display = "none";
+    thirdi.style.display = "none";
+    fourthi.style.display = "block";
+    fifthi.style.display = "none";
+}
+
+function displayDiv5() {
+    var first = document.getElementById("imagecontent1")
+    var second = document.getElementById("imagecontent2")
+    var third = document.getElementById("imagecontent3")
+    var fourth = document.getElementById("imagecontent4")
+    var fifth = document.getElementById("imagecontent5")
+
+    first.style.display = "none";
+    second.style.display = "none";
+    third.style.display = "none";
+    fourth.style.display = "none";
+    fifth.style.display = "block";
+
+    var firsti = document.getElementById("infocontent1")
+    var secondi = document.getElementById("infocontent2")
+    var thirdi = document.getElementById("infocontent3")
+    var fourthi = document.getElementById("infocontent4")
+    var fifthi = document.getElementById("infocontent5")
+
+    firsti.style.display = "none";
+    secondi.style.display = "none";
+    thirdi.style.display = "none";
+    fourthi.style.display = "none";
+    fifthi.style.display = "block";
+}
