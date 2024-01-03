d1ff0ba890023a35029cbced95b1a9d33986cfdd
elmullin <emily.mullins@comcast.net>
2024-01-03 15:53:15 -0800

    2024 update
    update

--- script2.js
+++ script2.js
@@ -5,6 +5,7 @@ function displayDiv1() {
     var fourth = document.getElementById("imagecontent4")
     var fifth = document.getElementById("imagecontent5")
     var sixth = document.getElementById("imagecontent6")
+    var seventh = document.getElementById("imagecontent7")
 
     first.style.display = "block";
     second.style.display = "none";
@@ -12,6 +13,7 @@ function displayDiv1() {
     fourth.style.display = "none";
     fifth.style.display = "none";
     sixth.style.display = "none";
+    seventh.style.display = "none";
 
 }
 
@@ -22,6 +24,7 @@ function displayInfo1() {
     var info4 = document.getElementById("infocontent4")
     var info5 = document.getElementById("infocontent5")
     var info6 = document.getElementById("infocontent6")
+    var info7 = document.getElementById("infocontent7")
 
     info1.style.display = "block";
     info2.style.display = "none";
@@ -29,6 +32,7 @@ function displayInfo1() {
     info4.style.display = "none";
     info5.style.display = "none";
     info6.style.display = "none";
+    info7.style.display = "none";
 
     displayDiv1();
 }
@@ -40,6 +44,7 @@ function displayDiv2() {
     var fourth = document.getElementById("imagecontent4")
     var fifth = document.getElementById("imagecontent5")
     var sixth = document.getElementById("imagecontent6")
+    var seventh = document.getElementById("imagecontent7")
 
     first.style.display = "none";
     second.style.display = "block";
@@ -47,6 +52,7 @@ function displayDiv2() {
     fourth.style.display = "none";
     fifth.style.display = "none";
     sixth.style.display = "none";
+    seventh.style.display = "none";
 
 }
 
@@ -57,6 +63,7 @@ function displayInfo2() {
     var info4 = document.getElementById("infocontent4")
     var info5 = document.getElementById("infocontent5")
     var info6 = document.getElementById("infocontent6")
+    var info7 = document.getElementById("infocontent7")
 
     info1.style.display = "none";
     info2.style.display = "block";
@@ -64,6 +71,7 @@ function displayInfo2() {
     info4.style.display = "none";
     info5.style.display = "none";
     info6.style.display = "none";
+    info7.style.display = "none";
 
     displayDiv2();
 }
@@ -75,6 +83,7 @@ function displayDiv3() {
     var fourth = document.getElementById("imagecontent4")
     var fifth = document.getElementById("imagecontent5")
     var sixth = document.getElementById("imagecontent6")
+    var seventh = document.getElementById("imagecontent7")
 
     first.style.display = "none";
     second.style.display = "none";
@@ -82,6 +91,7 @@ function displayDiv3() {
     fourth.style.display = "none";
     fifth.style.display = "none";
     sixth.style.display = "none";
+    seventh.style.display = "none";
 }
 
 function displayInfo3() {
@@ -91,6 +101,7 @@ function displayInfo3() {
     var info4 = document.getElementById("infocontent4")
     var info5 = document.getElementById("infocontent5")
     var info6 = document.getElementById("infocontent6")
+    var info7 = document.getElementById("infocontent7")
 
     info1.style.display = "none";
     info2.style.display = "none";
@@ -98,6 +109,7 @@ function displayInfo3() {
     info4.style.display = "none";
     info5.style.display = "none";
     info6.style.display = "none";
+    info7.style.display = "none";
 
     displayDiv3();
 }
@@ -109,6 +121,7 @@ function displayDiv4() {
     var fourth = document.getElementById("imagecontent4")
     var fifth = document.getElementById("imagecontent5")
     var sixth = document.getElementById("imagecontent6")
+    var seventh = document.getElementById("imagecontent7")
 
     first.style.display = "none";
     second.style.display = "none";
@@ -116,6 +129,7 @@ function displayDiv4() {
     fourth.style.display = "block";
     fifth.style.display = "none";
     sixth.style.display = "none";
+    seventh.style.display = "none";
 }
 
 function displayInfo4() {
@@ -125,6 +139,7 @@ function displayInfo4() {
     var info4 = document.getElementById("infocontent4")
     var info5 = document.getElementById("infocontent5")
     var info6 = document.getElementById("infocontent6")
+    var info7 = document.getElementById("infocontent7")
 
     info1.style.display = "none";
     info2.style.display = "none";
@@ -132,6 +147,7 @@ function displayInfo4() {
     info4.style.display = "block";
     info5.style.display = "none";
     info6.style.display = "none";
+    info7.style.display = "none";
 
     displayDiv4();
 }
@@ -143,6 +159,7 @@ function displayDiv5() {
     var fourth = document.getElementById("imagecontent4")
     var fifth = document.getElementById("imagecontent5")
     var sixth = document.getElementById("imagecontent6")
+    var seventh = document.getElementById("imagecontent7")
 
     first.style.display = "none";
     second.style.display = "none";
@@ -150,6 +167,7 @@ function displayDiv5() {
     fourth.style.display = "none";
     fifth.style.display = "block";
     sixth.style.display = "none";
+    seventh.style.display = "none";
 }
 
 function displayInfo5() {
@@ -159,6 +177,7 @@ function displayInfo5() {
     var info4 = document.getElementById("infocontent4")
     var info5 = document.getElementById("infocontent5")
     var info6 = document.getElementById("infocontent6")
+    var info7 = document.getElementById("infocontent7")
 
     info1.style.display = "none";
     info2.style.display = "none";
@@ -166,6 +185,7 @@ function displayInfo5() {
     info4.style.display = "none";
     info5.style.display = "block";
     info6.style.display = "none";
+    info7.style.display = "none";
 
 
     displayDiv5();
@@ -178,6 +198,7 @@ function displayDiv6() {
     var fourth = document.getElementById("imagecontent4")
     var fifth = document.getElementById("imagecontent5")
     var sixth = document.getElementById("imagecontent6")
+    var seventh = document.getElementById("imagecontent7")
 
     first.style.display = "none";
     second.style.display = "none";
@@ -185,6 +206,7 @@ function displayDiv6() {
     fourth.style.display = "none";
     fifth.style.display = "none";
     sixth.style.display = "block";
+    seventh.style.display = "none";
 }
 
 function displayInfo6() {
@@ -194,6 +216,7 @@ function displayInfo6() {
     var info4 = document.getElementById("infocontent4")
     var info5 = document.getElementById("infocontent5")
     var info6 = document.getElementById("infocontent6")
+    var info7 = document.getElementById("infocontent7")
 
     info1.style.display = "none";
     info2.style.display = "none";
@@ -201,6 +224,45 @@ function displayInfo6() {
     info4.style.display = "none";
     info5.style.display = "none";
     info6.style.display = "block";
+    info7.style.display = "none";
+
+    displayDiv6();
+}
+
+function displayDiv7() {
+    var first = document.getElementById("imagecontent1")
+    var second = document.getElementById("imagecontent2")
+    var third = document.getElementById("imagecontent3")
+    var fourth = document.getElementById("imagecontent4")
+    var fifth = document.getElementById("imagecontent5")
+    var sixth = document.getElementById("imagecontent6")
+    var seventh = document.getElementById("imagecontent7")
+
+    first.style.display = "none";
+    second.style.display = "none";
+    third.style.display = "none";
+    fourth.style.display = "none";
+    fifth.style.display = "none";
+    sixth.style.display = "none";
+    seventh.style.display = "block";
+}
+
+function displayInfo7() {
+    var info1 = document.getElementById("infocontent1")
+    var info2 = document.getElementById("infocontent2")
+    var info3 = document.getElementById("infocontent3")
+    var info4 = document.getElementById("infocontent4")
+    var info5 = document.getElementById("infocontent5")
+    var info6 = document.getElementById("infocontent6")
+    var info7 = document.getElementById("infocontent7")
+
+    info1.style.display = "none";
+    info2.style.display = "none";
+    info3.style.display = "none";
+    info4.style.display = "none";
+    info5.style.display = "none";
+    info6.style.display = "none";
+    info7.style.display = "block";
 
     displayDiv6();
 }
